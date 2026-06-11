552ca9d16ed2c22e653f103e4f03c0ad03971b9c
imzed01 <heavensdigitalmarketing@gmail.com>
2026-06-12 02:07:51 +0500

    Update setup-admin.js

--- pages/api/setup-admin.js
+++ pages/api/setup-admin.js
@@ -1,8 +1,9 @@
-const dbConnect = require('../../lib/dbConnect').default;
-const User = require('../../models/User').default;
-const bcrypt = require('bcryptjs');
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../lib/dbConnect';
+import User from '../../models/User';
+import bcrypt from 'bcryptjs';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.secret !== 'setup-taha-2024') {
     return res.status(401).json({ message: 'Unauthorized' });
   }
@@ -23,7 +24,7 @@ export default async function handler(req, res) {
     });
 
     return res.status(200).json({ message: 'Admin created successfully!' });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ message: 'Error', error: error.message });
   }
 }
